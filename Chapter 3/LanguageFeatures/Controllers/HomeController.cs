52e13c87eda2478fe92a7b9be645c51d7ad2e1b9
mac-max <luca.anghileri@ff3d.com>
2014-11-26 14:32:35 +0100

    After Update

---
+++ Chapter 3/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public String Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ActionResult AutoProperty()
+        {
+            //create a new Product object
+            Product myProduct = new Product();
+
+            //set name product
+            myProduct.Name = "Kayak";
+
+            //get the property
+            string productName = myProduct.Name;
+
+            //generate the view
+            return View("Result", (object)String.Format("Product Name: {0}", productName));
+        }
+
+	}
+}
