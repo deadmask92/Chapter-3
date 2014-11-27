1c918a575c4e0b8ccfa27629167f83983fea6952
deadmask <luca.anghileri@gmail.com>
2014-11-27 01:17:49 +0100

    Example with LINQ

--- Chapter 3/LanguageFeatures/Controllers/HomeController.cs
+++ Chapter 3/LanguageFeatures/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LanguageFeatures.Models;
+using System.Text;
 
 namespace LanguageFeatures.Controllers
 {
@@ -29,6 +30,237 @@ namespace LanguageFeatures.Controllers
 
             //generate the view
             return View("Result", (object)String.Format("Product Name: {0}", productName));
+        }
+
+        public ActionResult CreateProduct()
+        {
+            //Create a new Product object
+            Product myProduct = new Product();
+
+            //set property value
+            myProduct.ProductID = 100;
+            myProduct.Name = "Kayak";
+            myProduct.Description = "A bot for one person";
+            myProduct.Price = 275M;
+            myProduct.Category = "Watersport";
+
+            return View("Result", (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ActionResult CreateProduct2()
+        {
+            //create and populate a new Product object
+            Product myProduct = new Product
+            {
+                ProductID = 100,
+                Name = "Kayak",
+                Description = "A bot for one person",
+                Price = 275M,
+                Category = "Watersport"
+            };
+            return View("Result", (object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+
+            Dictionary<string, int> myDict = new Dictionary<string, int>
+            {
+                {"apple", 10}, {"orange", 20}, {"plum", 30}
+            };
+
+            return View("Result", (object)stringArray[1]);
+
+        }
+
+        public ViewResult UseExtension()
+        {
+            //crate and populate ShoppingCart obj
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>{
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "Lifejacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.9M},
+                }
+            };
+
+            //get the total value of the products in the cart
+            decimal cartTotal = cart.TotalPrices();
+
+            return View("Result", (object)String.Format("Total: {0:c}", cartTotal));
+
+        }
+
+        public ViewResult UseExtensionEnumerable()
+        {
+
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name = "Kayak", Price = 275M},
+                    new Product {Name = "Lifejacket", Price = 48.95M},
+                    new Product {Name = "Soccer ball", Price = 19.50M},
+                    new Product {Name = "Corner flag", Price = 34.9M},
+                }
+            };
+
+            //create and populate an array of Product object
+            Product[] productArray = {
+                                        new Product {Name = "Kayak", Price = 275M},
+                                        new Product {Name = "Lifejacket", Price = 48.95M},
+                                        new Product {Name = "Soccer ball", Price = 19.50M},
+                                        new Product {Name = "Corner flag", Price = 34.9M},
+                                     };
+
+            //get the total value of the products in the cart
+            decimal cartTotal = products.TotalPrices();
+            decimal arrayTotal = productArray.TotalPrices();
+
+            return View("Result", (object)String.Format("Cart Total: {0}, ArrayTotal: {1}", cartTotal, arrayTotal));
+
+        }
+
+        public ViewResult UseFilterExtensionMethod()
+        {
+            IEnumerable<Product> products = new ShoppingCart 
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name ="Kayak", Category = "Watersport", Price = 275M},
+                    new Product {Name ="Lifejacket", Category = "Watersport", Price = 48.95M},
+                    new Product {Name ="Soccer ball", Category = "Soccer", Price = 19.50M},
+                    new Product {Name ="Corner flas", Category = "Soccer", Price = 34.95M},
+                }
+            };
+
+            decimal total = 0;
+
+            foreach (Product prod in products.FilterByCategory("Soccer"))
+            {
+                total += prod.Price;
+            }
+
+            return View("Result", (object)String.Format("Total: {0}", total));
+
+        }
+
+        public ViewResult UseFilterExtensionMethod2()
+        {
+            IEnumerable<Product> products = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product {Name ="Kayak", Category = "Watersport", Price = 275M},
+                    new Product {Name ="Lifejacket", Category = "Watersport", Price = 48.95M},
+                    new Product {Name ="Soccer ball", Category = "Soccer", Price = 19.50M},
+                    new Product {Name ="Corner flas", Category = "Soccer", Price = 34.95M},
+                }
+            };
+
+            decimal total = 0;
+
+            //first way to create a delegate for filter extension method
+            Func<Product, bool> categoryFilter = delegate(Product prod) {
+                return prod.Category == "Soccer";            
+            };
+
+            //second way to crate a delegate for filter extension method --> using LMBDA expression
+            Func<Product, bool> categoryFilter2 = prod => prod.Category == "Soccer";
+
+            //call filter extension method create a func variable
+            foreach (Product prod in products.Filter(categoryFilter))
+            {
+                total += prod.Price;
+            }
+
+            //call filter extension method without create a fucn variable
+            foreach (Product prod in products.Filter(prod => prod.Category == "Soccer"))
+            {
+                total += prod.Price;
+            }
+
+            //filter IEnumerable with more than one condition
+            foreach (Product prod in products.Filter(prod => prod.Category == "Soccer" || prod.Price > 20))
+            {
+                total += prod.Price;
+            }
+
+            return View("Result", (object)String.Format("Total: {0}", total));
+        }
+
+        public ActionResult CreateAnonArray()
+        {
+
+            var oddsAndEnds = new[] {
+                new { Name = "MVC", Category = "Pattern"},
+                new { Name = "Hat", Category = "Clothing"},
+                new { Name = "Apple", Category = "Fruit"}
+            };
+
+            StringBuilder result = new StringBuilder();
+            foreach (var item in oddsAndEnds)
+            {
+                result.Append(item.Name).Append("");
+            }
+
+            return View("Result", (Object)result.ToString());
+
+        }
+
+        public ActionResult FindProductsWithLinq()
+        {
+            Product[] products = {
+                new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            };
+
+            var foundProducts = from match in products
+                                orderby match.Price descending
+                                select new { match.Name, match.Price };
+
+            //crate the result
+            //find the first 3 products
+            int count = 0;
+            StringBuilder result = new StringBuilder();
+            foreach (var p in foundProducts)
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+                if (++count == 3)
+                {
+                    break;
+                }
+            }
+
+            return View("Result", (object)result.ToString());
+        }
+
+        public ActionResult FindProductsWithLinqDotNotification()
+        {
+            Product[] products = {
+                new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+                new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+                new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+                new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+            };
+
+            var foundProducts = products.OrderByDescending(e => e.Price).Take(3).Select(e => new { e.Name, e.Price });
+
+            StringBuilder result = new StringBuilder();
+            foreach (var p in foundProducts)
+            {
+                result.AppendFormat("Price: {0} ", p.Price);
+            }
+
+            return View("Result", (object)result.ToString());
+
         }
 
 	}
--- Chapter 3/LanguageFeatures/Models/Product.cs
+++ Chapter 3/LanguageFeatures/Models/Product.cs
@@ -7,12 +7,40 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
+        private int productID;
         private string name;
+        private string description;
+        private decimal price;
+        private string category;
+
+        public int ProductID
+        {
+            get { return productID; }
+            set { productID = value; }
+        }
 
         public string Name
         {
-            get { return name; }
+            get { return productID + name; }
             set { name = value; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; }
         }
 
     }
